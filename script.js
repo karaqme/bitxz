9e5ef9d4d11d8b2ef831905e6bfe8db6b3b5b851
karaqme <rigovsema@mail.ru>
2024-07-30 11:31:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let tg = window.Telegram.WebApp;
 tg.expand();
 
-if (tg && tg.initData && tg.initData.user) {
-    const firstName = tg.initData.user.first_name;
-    document.getElementById('user_name').innerText = firstName;
-  } else {
-    console.error('Error: Failed to retrieve user data');
-  }
+const firstName = tg.initData.user.first_name;
+document.getElementById('user_name').innerText = firstName;
